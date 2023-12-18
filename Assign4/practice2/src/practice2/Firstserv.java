04a2b14c18ae609229aa5dd6895c650cddeb133b
nup20 <nupurthakre16@gmail.com>
2023-12-18 17:49:04 +0530

    .

---
+++ Assign4/practice2/src/practice2/Firstserv.java
@@ -0,0 +1,28 @@
+package practice2;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Firstserv extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		resp.setContentType("text/html");
+		PrintWriter out=resp.getWriter();
+		out.print("welcom to <b>server</b>");
+		System.out.print("hello world");
+		
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+	}
+
+}
