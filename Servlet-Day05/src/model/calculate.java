b02e56e47615928d29817ac3ec1b620146c1d31a
nup20 <130826435+nup20@users.noreply.github.com>
2024-02-21 08:41:56 +0530

    Add files via upload

---
+++ Servlet-Day05/src/model/calculate.java
@@ -0,0 +1,33 @@
+package model;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/calculate")
+public class calculate extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	response.setContentType("text/html");
+	PrintWriter out=response.getWriter();
+	int a=Integer.parseInt(request.getParameter("n1"));
+	int b=Integer.parseInt(request.getParameter("n2"));
+	
+	
+response.sendRedirect("http://www.google.com");
+	
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doGet(request, response);
+	}
+
+}
